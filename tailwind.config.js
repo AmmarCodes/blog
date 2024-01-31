a217b62daa968c5e20ce39834d49a7ee6e97d0a3
Ammar Alakkad <am.alakkad@gmail.com>
2024-01-31 13:14:19 +0300

    tailwind: adjust container width

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,25 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        ".container": {
+          maxWidth: "100%",
+          "@screen sm": {
+            maxWidth: "600px",
+          },
+          "@screen md": {
+            maxWidth: "700px",
+          },
+          "@screen lg": {
+            maxWidth: "800px",
+          },
+          "@screen xl": {
+            maxWidth: "900px",
+          },
+        },
+      });
+    },
+  ],
 };
