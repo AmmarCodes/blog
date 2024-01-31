199563c102c025d3767fc5cc2517790ebe88e397
Ammar Alakkad <am.alakkad@gmail.com>
2024-01-31 13:14:54 +0300

    eleventy: update permalink structure

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,12 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./public/*.css");
   eleventyConfig.addPassthroughCopy("src/images");
 
+  // https://www.11ty.dev/docs/data-eleventy-supplied/#changing-your-project-default-permalinks
+  eleventyConfig.addGlobalData("permalink", () => {
+    return (data) =>
+      `${data.page.filePathStem}.${data.page.outputFileExtension}`;
+  });
+
   return {
     dir: {
       input: "src",
