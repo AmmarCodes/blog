6ee2bb0c5ef3a888ab72f05a2b881e3ea1c1f726
Ammar Alakkad <am.alakkad@gmail.com>
2024-02-25 18:26:03 +0300

    Add date to post

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,21 @@
+const { DateTime } = require("luxon");
 module.exports = function (eleventyConfig) {
   eleventyConfig.setUseGitIgnore(false);
   eleventyConfig.addWatchTarget("./public/*.css");
   eleventyConfig.addPassthroughCopy("src/images");
 
-  //www.11ty.dev/docs/data-eleventy-supplied/#changing-your-project-default-permalinks
-  https: eleventyConfig.addGlobalData("permalink", () => {
+  // https://www.11ty.dev/docs/data-eleventy-supplied/#changing-your-project-default-permalinks
+  eleventyConfig.addGlobalData("permalink", () => {
     return (data) =>
       `${data.page.fileSlug}/index.${data.page.outputFileExtension}`;
+  });
+
+  eleventyConfig.addFilter("htmlDateString", (dateObj) => {
+    // dateObj input: https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
+      "MMM dd, yyyy",
+      // "yyyy-MM-dd",
+    );
   });
 
   return {
