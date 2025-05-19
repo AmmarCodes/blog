822d312998ff2badf05f232c23bd6a6474c6fe5e
Ammar Alakkad <am.alakkad@gmail.com>
2025-05-19 09:37:22 +0300

    Upgrade tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,28 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,njk}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    function ({ addComponents }) {
-      addComponents({
-        ".container": {
-          maxWidth: "100%",
-          "@screen sm": {
-            maxWidth: "600px",
-          },
-          "@screen md": {
-            maxWidth: "700px",
-          },
-          "@screen lg": {
-            maxWidth: "800px",
-          },
-          "@screen xl": {
-            maxWidth: "900px",
-          },
-        },
-      });
-    },
-  ],
-};
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["./src/**/*.{html,njk}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [
+//     function ({ addComponents }) {
+//       addComponents({
+//         ".container": {
+//           maxWidth: "100%",
+//           "@screen sm": {
+//             maxWidth: "600px",
+//           },
+//           "@screen md": {
+//             maxWidth: "700px",
+//           },
+//           "@screen lg": {
+//             maxWidth: "800px",
+//           },
+//           "@screen xl": {
+//             maxWidth: "900px",
+//           },
+//         },
+//       });
+//     },
+//   ],
+// };
